297fcf89eca1d5dd62a32854419c1f400a017393
SchattenWolf2008 <31274402+SchattenWolf2008@users.noreply.github.com>
2025-08-09 21:48:25 +0200

    Update entrypoint.sh
    + debug whoami && echo $HOME && pwd && ls -la

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ export HOME="${HOME:-/home/container}"
 # 2) Always run SteamCMD from $HOME (where it was extracted in the Dockerfile)
 cd "$HOME"
 
+#Debug Home
+whoami && echo $HOME && pwd && ls -la
+
 # 3) Optional: workshop scenario download via STEAMCMD runscript
 #    (If SCENARIO_WORKSHOP_ID is set, append a +runscript argument)
 if [ -n "$SCENARIO_WORKSHOP_ID" ]; then
